515f44169c9fe066fef7120430733febfb2ae341
Nikita Bolshakov <bnikmik@gmail.com>
2022-06-22 22:12:44 +0300

    homework1.7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String fullName = "алена Владимировна большакова";
+
+        char element = ' ';
+        int indexA = fullName.indexOf(element);
+        int indexB = fullName.lastIndexOf(element);
+        int indexC = fullName.length();
+
+
+        String lastName = fullName.substring(0, indexA);
+        String firstName = fullName.substring(indexA+1, indexB);
+        String middleName = fullName.substring(indexB + 1, indexC);
+
+        System.out.println("Имя сотрудника — " + firstName);
+        System.out.println("Фамилия сотрудника — " + lastName);
+        System.out.println("Отчество сотрудника — " + middleName);
+
+        System.out.println(" ");
+
+        char[] a = fullName.toCharArray();
+        for (int i = 0; i < a.length; i++) {
+            if (i==0) {
+                System.out.print(Character.toUpperCase(a[i]));
+            }
+            else if (a[i]==' ') {
+                i+=1;
+                System.out.print(" " + Character.toUpperCase(a[i]));
+            }
+            else System.out.print(a[i]);
+        }
     }
 }
