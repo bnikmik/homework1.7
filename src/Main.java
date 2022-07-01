ac5542e306f44d32186c439de8664f67a859a731
Nikita Bolshakov <bnikmik@gmail.com>
2022-07-01 13:44:15 +0300

    homework 1.7

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String fullName = "алена Владимировна большакова";
+        String fullName = "Большаков Никита Михайлович";
 
         char element = ' ';
         int indexA = fullName.indexOf(element);
@@ -19,6 +19,7 @@ public class Main {
         System.out.println(" ");
 
         char[] a = fullName.toCharArray();
+
         for (int i = 0; i < a.length; i++) {
             if (i==0) {
                 System.out.print(Character.toUpperCase(a[i]));
@@ -28,6 +29,31 @@ public class Main {
                 System.out.print(" " + Character.toUpperCase(a[i]));
             }
             else System.out.print(a[i]);
+        }
+        System.out.println();
+
+        String firstS = "1357";
+        String secondS = "2468";
+
+        StringBuilder sbF = new StringBuilder(firstS);
+        StringBuilder sbS = new StringBuilder(secondS);
+
+        int sbLengthF = sbF.length();
+        int sbLengthS = sbS.length();
+
+        for (int i = 1, j=0; i < sbLengthF || j < sbLengthS; i+=2,j++) {
+            sbF.insert(i,sbS.charAt(j));
+        }
+        System.out.println(sbF);
+
+        System.out.println();
+
+        String strVariantA = "aabccddefghhiijjkk";
+        char[] d = strVariantA.toCharArray();
+        for (int i = 0 , j=1; i < d.length && j <= d.length-1;  i++,j++) {
+            if (d[i] == d[j]) {
+                System.out.print(d[i]);
+            }
         }
     }
 }
